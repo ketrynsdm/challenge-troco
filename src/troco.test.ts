ea572ed1c72e3e1ae94c987c7abf487471eadb85
Ketryn Silva <ketryn29@gmail.com>
2022-04-25 14:13:00 -0300

    testando notas e moedas

--- src/troco.test.ts
+++ src/troco.test.ts
@@ -10,9 +10,41 @@ describe('Testando as funcionalidades do Troco', () => {
         const result = { "0.05": 0, "0.1": 0, "0.25": 0, "0.5": 0, "1": 0, "10": 1, "100": 0, "2": 0, "20": 0, "5": 0, "50": 0 }
         expect(moedas(10)).toMatchObject(result);
     });
+    it('Deve receber a quantidade de notas e moedas de: ', () => {
+        const result = { "0.05": 1, "0.1": 0, "0.25": 0, "0.5": 0, "1": 0, "10": 1, "100": 0, "2": 0, "20": 0, "5": 0, "50": 0 }
+        expect(moedas(10.05)).toMatchObject(result);
+    });
+    it('Deve receber a quantidade de notas e moedas de: ', () => {
+        const result = { "0.05": 0, "0.1": 1, "0.25": 0, "0.5": 0, "1": 0, "10": 1, "100": 0, "2": 0, "20": 0, "5": 0, "50": 0 }
+        expect(moedas(10.1)).toMatchObject(result);
+    });
+    it('Deve receber a quantidade de notas e moedas de: ', () => {
+        const result = { "0.05": 0, "0.1": 0, "0.25": 1, "0.5": 0, "1": 0, "10": 1, "100": 0, "2": 0, "20": 0, "5": 0, "50": 0 }
+        expect(moedas(10.25)).toMatchObject(result);
+    });
     it('Deve receber a quantidade de notas e moedas', () => {
         const result = { "0.05": 0, "0.1": 0, "0.25": 0, "0.5": 1, "1": 0, "10": 1, "100": 0, "2": 0, "20": 0, "5": 0, "50": 0 }
         expect(moedas(10.5)).toMatchObject(result);
+    });
+    it('Deve receber a quantidade de notas e moedas', () => {
+        const result = { "0.05": 0, "0.1": 0, "0.25": 0, "0.5": 0, "1": 1, "10": 1, "100": 0, "2": 0, "20": 0, "5": 0, "50": 0 }
+        expect(moedas(11)).toMatchObject(result);
+    });
+    it('Deve receber a quantidade de notas e moedas', () => {
+        const result = { "0.05": 0, "0.1": 0, "0.25": 0, "0.5": 0, "1": 0, "10": 0, "100": 0, "2": 1, "20": 0, "5": 0, "50": 0 }
+        expect(moedas(2)).toMatchObject(result);
+    });
+    it('Deve receber a quantidade de notas e moedas', () => {
+        const result = { "0.05": 0, "0.1": 0, "0.25": 0, "0.5": 0, "1": 0, "10": 0, "100": 0, "2": 0, "20": 1, "5": 0, "50": 0 }
+        expect(moedas(20)).toMatchObject(result);
+    });
+    it('Deve receber a quantidade de notas e moedas', () => {
+        const result = { "0.05": 0, "0.1": 0, "0.25": 0, "0.5": 0, "1": 0, "10": 0, "100": 0, "2": 0, "20": 0, "5": 1, "50": 0 }
+        expect(moedas(5)).toMatchObject(result);
+    });
+    it('Deve receber a quantidade de notas e moedas', () => {
+        const result = { "0.05": 0, "0.1": 0, "0.25": 0, "0.5": 0, "1": 0, "10": 0, "100": 0, "2": 0, "20": 0, "5": 0, "50": 1 }
+        expect(moedas(50)).toMatchObject(result);
     });
 
     it('Valor do produto subtraido do valor pago', () => {
